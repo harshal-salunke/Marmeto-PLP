772060a77976a491226f5d240d2df883879048af
unknown <harshalsalunke23@gmail.com>
2023-07-07 16:19:51 +0530

    first commit

---
+++ src/screens/ListView/ListView.jsx
@@ -0,0 +1,222 @@
+import React from "react";
+import "./style.css";
+
+export const ListView = () => {
+  return (
+    <div className="LIST-VIEW">
+      <div className="div">
+        <div className="overlap">
+          <div className="group">
+            <div className="overlap-group">
+              <div className="product-card">
+                <div className="overlap-2">
+                  <img
+                    className="rectangle"
+                    alt="Rectangle"
+                    src="https://generation-sessions.s3.amazonaws.com/e7b3076702d31b4f2b8c20edab667c89/img/rectangle-4-1@2x.png"
+                  />
+                  <div className="text-wrapper">UM TRAINING</div>
+                  <div className="varinat">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-2">RED / S</div>
+                    </div>
+                  </div>
+                  <div className="overlap-wrapper">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-3">RED / M</div>
+                    </div>
+                  </div>
+                  <div className="overlap-group-wrapper">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-4">RED / L</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <img
+                className="img"
+                alt="Rectangle"
+                src="https://generation-sessions.s3.amazonaws.com/e7b3076702d31b4f2b8c20edab667c89/img/rectangle-4-1@2x.png"
+              />
+              <img
+                className="img"
+                alt="Rectangle"
+                src="https://generation-sessions.s3.amazonaws.com/e7b3076702d31b4f2b8c20edab667c89/img/rectangle-4-1@2x.png"
+              />
+              <div className="rectangle-2" />
+              <div className="text-wrapper-5">NEW</div>
+            </div>
+          </div>
+          <div className="group-2">
+            <div className="overlap-3">
+              <div className="overlap-4">
+                <div className="rectangle-3" />
+                <div className="text-wrapper-6">NEW</div>
+              </div>
+              <div className="text-wrapper">LIMITED</div>
+              <div className="varinat">
+                <div className="div-wrapper">
+                  <div className="text-wrapper-2">BLUE / S</div>
+                </div>
+              </div>
+              <div className="overlap-wrapper">
+                <div className="div-wrapper">
+                  <div className="text-wrapper-2">BLUE / M</div>
+                </div>
+              </div>
+              <div className="overlap-group-wrapper">
+                <div className="div-wrapper">
+                  <div className="text-wrapper-7">BLUE / L</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="group-3">
+            <div className="overlap-group">
+              <div className="product-card">
+                <div className="overlap-3">
+                  <img
+                    className="rectangle"
+                    alt="Rectangle"
+                    src="https://generation-sessions.s3.amazonaws.com/e7b3076702d31b4f2b8c20edab667c89/img/rectangle-3-5@2x.png"
+                  />
+                  <div className="text-wrapper">BEACH BUM</div>
+                  <div className="varinat">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-8">ORANGE / S</div>
+                    </div>
+                  </div>
+                  <div className="overlap-wrapper">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-9">ORANGE / M</div>
+                    </div>
+                  </div>
+                  <div className="varinat-2">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-10">ORANGE / L</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="rectangle-2" />
+              <div className="text-wrapper-5">NEW</div>
+            </div>
+          </div>
+          <div className="group-4">
+            <div className="overlap-group">
+              <div className="product-card">
+                <div className="overlap-3">
+                  <img
+                    className="rectangle"
+                    alt="Rectangle"
+                    src="https://generation-sessions.s3.amazonaws.com/e7b3076702d31b4f2b8c20edab667c89/img/rectangle-3-6@2x.png"
+                  />
+                  <div className="text-wrapper">LIMITED X</div>
+                  <div className="varinat">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-2">BLUE / XS</div>
+                    </div>
+                  </div>
+                  <div className="overlap-wrapper">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-2">BLUE / M</div>
+                    </div>
+                  </div>
+                  <div className="overlap-group-wrapper">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-7">BLUE / XL</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="rectangle-4" />
+              <div className="text-wrapper-11">NEW</div>
+            </div>
+          </div>
+          <div className="group-5">
+            <div className="overlap-group">
+              <div className="product-card">
+                <div className="overlap-3">
+                  <div className="rectangle-wrapper">
+                    <img
+                      className="rectangle-5"
+                      alt="Rectangle"
+                      src="https://generation-sessions.s3.amazonaws.com/e7b3076702d31b4f2b8c20edab667c89/img/rectangle-3-5@2x.png"
+                    />
+                  </div>
+                  <div className="text-wrapper-12">BEACH BUM X</div>
+                  <div className="varinat">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-8">ORANGE / XS</div>
+                    </div>
+                  </div>
+                  <div className="overlap-wrapper">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-9">ORANGE / M</div>
+                    </div>
+                  </div>
+                  <div className="varinat-2">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-10">ORANGE / XL</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="rectangle-2" />
+              <div className="text-wrapper-5">NEW</div>
+            </div>
+          </div>
+          <div className="group-wrapper">
+            <div className="group-6">
+              <div className="overlap-group">
+                <div className="product-card">
+                  <div className="overlap-3">
+                    <img
+                      className="rectangle"
+                      alt="Rectangle"
+                      src="https://generation-sessions.s3.amazonaws.com/e7b3076702d31b4f2b8c20edab667c89/img/rectangle-4-1@2x.png"
+                    />
+                    <div className="text-wrapper">UM TRAINING X</div>
+                    <div className="varinat">
+                      <div className="div-wrapper">
+                        <div className="text-wrapper-2">RED / XS</div>
+                      </div>
+                    </div>
+                    <div className="overlap-wrapper">
+                      <div className="div-wrapper">
+                        <div className="text-wrapper-3">RED / M</div>
+                      </div>
+                    </div>
+                    <div className="overlap-group-wrapper">
+                      <div className="div-wrapper">
+                        <div className="text-wrapper-4">RED / XL</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="rectangle-2" />
+                <div className="text-wrapper-13">NEW</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h1 className="h-1">PLP</h1>
+        <div className="search-bar">
+          <div className="p-wrapper">
+            <p className="p">Type something to search ...</p>
+          </div>
+        </div>
+        <img
+          className="menu"
+          alt="Menu"
+          src="https://generation-sessions.s3.amazonaws.com/e7b3076702d31b4f2b8c20edab667c89/img/menu-1@2x.png"
+        />
+        <img
+          className="list"
+          alt="List"
+          src="https://generation-sessions.s3.amazonaws.com/e7b3076702d31b4f2b8c20edab667c89/img/list-1@2x.png"
+        />
+      </div>
+    </div>
+  );
+};
